b9a851eef4fcecac6ecce5495ad50f6c17a2d387
小河柊太 <shuta800@ogawahiiragifutoshinoMacBook-Pro.local>
2025-10-15 16:18:22 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,11 +1,13 @@
-Vec3 = function( x, y, z )
+class Vec3
 {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-}
+    constructor( x, y, z )
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
-Vec3.prototype.add = function( v )
+add( v )
 {
     this.x += v.x;
     this.y += v.y;
@@ -13,7 +15,7 @@ Vec3.prototype.add = function( v )
     return this;
 }
 
-Vec3.prototype.sub = function( v )
+sub( v )
 {
     this.x -= v.x;
     this.y -= v.y;
@@ -21,39 +23,37 @@ Vec3.prototype.sub = function( v )
     return this;
 }
 
-Vec3.prototype.sum = function()
+sum()
 {
     return this.x + this.y + this.z;
 }
 
-Vec3.prototype.min = function()
+min()
 {
-    //return Math.min( this.x, this.y, this.z );
-    const m =  this.x < this.y ? this.x : this.y;
-    return m < this.z ? m : this.z;
+    return Math.min( this.x, this.y, this.z );
 }
 
-Vec3.prototype.max = function()
+max()
 {
-    //return Math.max( this.x, this.y, this.z );
-    const m = this.x > this.y ? this.x : this.y;
-    return m > this.z ? this.z : m;}
+    return Math.max( this.x, this.y, this.z );
+}
 
-Vec3.prototype.mid = function()
+mid()
 {
     return this.sum() - this.min() - this.max();
 }
 
-Vec3.prototype.cross = function( v )
+cross( v )
 {
-    var x = this.x, y = this.y, z = this.z;
+    const x = this.x, y = this.y, z = this.z;
     this.x = y * v.z - z * v.y;
     this.y = z * v.x - x * v.z;
     this.z = x * v.y - y * v.x;
     return this;
 }
 
-Vec3.prototype.length = function()
+length()
 {
     return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+}
 }
