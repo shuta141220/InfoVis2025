9b1c595e7a880e9c5dcc392a18f62141791c4cbf
小河柊太 <shuta800@ogawahiiragifutoshinoMacBook-Pro.local>
2025-12-04 12:02:05 +0900

    Task2

--- W08/Task2_main.js
+++ W08/Task2_main.js
@@ -50,12 +50,6 @@ class LineChart {
         self.yscale = d3.scaleLinear()
             .range([self.inner_height, 0]);
 
-        self.xscale = d3.scaleLinear()
-            .range([0, self.inner_width]);
-
-        self.yscale = d3.scaleLinear()
-            .range([self.inner_height, 0]);
-
         self.xaxis = d3.axisBottom(self.xscale).ticks(5);
         self.yaxis = d3.axisLeft(self.yscale).ticks(5);
 
